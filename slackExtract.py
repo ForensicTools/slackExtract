9f6d5c899f01defa47dd536d7942e1f3897ece6c
Brett Fitzpatrick <brett@Bretts-MacBook-Pro-2.local>
2018-11-15 20:29:57 -0500

    Added slackExtract File

---
+++ slackExtract.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+slackExtract - A forensics tool to extract data from slack space
+Contributors: Brett Fitzpatrick, Michael Leardi
+Filename: slackExtract.py
+Purpose: Extracts data from slack space
+"""
+
+# libs
+
+
+######## START FUNCTIONS ########
