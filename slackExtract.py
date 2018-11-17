cdb97bf8a3ae52d2fd8418ec25932e635a9eea09
Brett Fitzpatrick <brett@Bretts-MacBook-Pro-2.local>
2018-11-16 21:32:45 -0500

    Added main function, argument parser, file checker

--- slackExtract.py
+++ slackExtract.py
@@ -10,3 +10,29 @@ Purpose: Extracts data from slack space
 
 
 ######## START FUNCTIONS ########
+
+def readSlackSpace(file, inode):
+	# TODO
+
+def is_valid_file(file):
+	# Checking if file exitst
+	try:
+		os.stat(file)
+	except FileNotFoundError as e:
+		print("Error 0: File appears to not exist.")
+		exit()
+
+def main(args):
+	# TODO
+	is_valid_file(args.file)
+
+
+if __name__=="__main__":
+	__parser__ = argparse.ArgumentParser(
+		description='Extracts slack from files'
+	)
+	__parser__.add_argument(
+	'-f', '--file', default=None, required=True, help='Input file name'
+	)
+	__args__ = __parser__.parse_args()
+	main(__args__)
